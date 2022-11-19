a4dc178f28923ab762652ebd8f28e61218e62cae
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-11-20 00:21:05 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName (){
+    customerName =  customerName.toUpperCase()
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+//overwriting
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'max'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
